7cade7c1bf73a3f76f74c86d0b906f4200e4719e
yficklis <yficklis.santos@gmail.com>
2024-04-01 15:08:45 -0300

    settings and drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,7 +16,7 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Home"),
         centerTitle: true,
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
